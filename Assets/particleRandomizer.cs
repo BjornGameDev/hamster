ceb43b86969a42c3951d8bfb7983dbf0ff215202
aarree <aarreentertainment@gmail.com>
2019-02-16 17:39:11 +0200

    hamster

--- Assets/particleRandomizer.cs
+++ Assets/particleRandomizer.cs
@@ -4,25 +4,35 @@ using UnityEngine;
 
 public class particleRandomizer : MonoBehaviour
 {
-    public QuantumManager qm;
+    public LineRenderer lr;
 
-    int sizeIndex;
-    int speedIndex;
-    int emissionIndex;
-    int rindex;
-    int gindex;
-    int bindex;
-
-
+    public int sizeIndex;
+    public int speedIndex;
+    public int emissionIndex;
+    public int rindex;
+    public int gindex;
+    public int bindex;
+    ParticleSystem.EmissionModule emod;
+    ParticleSystem.MainModule main;
     // Start is called before the first frame update
     void Start()
     {
+        ParticleSystem parts = GetComponent<ParticleSystem>();
+        emod = parts.emission;
+        main = parts.main;
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-            
+
+        if (lr.positionCount<1) 
+            return;
+
+
+        main.startSize = float.Parse(Mathf.Abs(lr.GetPosition(sizeIndex).y).ToString().Split(',')[0]);
+        main.startSpeed = float.Parse(Mathf.Abs(lr.GetPosition(speedIndex).y).ToString().Split(',')[0]);
+        emod.rateOverTimeMultiplier = float.Parse(Mathf.Abs(lr.GetPosition(emissionIndex).y).ToString().Split(',')[0]);
     }
 }
