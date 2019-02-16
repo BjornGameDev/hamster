b0d63b69e97bc57b6a137e3474c3e00da0ffa051
aarree <aarreentertainment@gmail.com>
2019-02-16 21:55:17 +0200

    big

---
+++ Assets/anyKey.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class anyKey : MonoBehaviour
+{
+    public string loadableScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(activator());
+    }
+    float startTimer = 0;
+    bool canchange = false;
+    IEnumerator activator()
+    {
+        yield return new WaitForEndOfFrame();
+        startTimer += Time.deltaTime;
+        if (startTimer >= 2)
+        {
+            canchange = true;
+        }
+        else
+        {
+            if(startTimer > 1)
+            {
+                Color col = GetComponent<UnityEngine.UI.Image>().color;
+                col.a -= Time.deltaTime;
+                GetComponent<UnityEngine.UI.Image>().color = col;
+            }
+            StartCoroutine(activator());
+        }
+        
+}
+
+    IEnumerator changeScene()
+    {
+        yield return new WaitForEndOfFrame();
+        Color col = GetComponent<UnityEngine.UI.Image>().color;
+        col.a += Time.deltaTime;
+        GetComponent<UnityEngine.UI.Image>().color = col;
+        if (col.a>=1){
+            UnityEngine.SceneManagement.SceneManager.LoadScene(loadableScreen);
+        }
+        else
+        {
+            StartCoroutine(changeScene());
+        }
+    }
+
+    // Update is called once per frame
+public void change()
+    {
+
+            StartCoroutine(changeScene());
+
+    }
+}
---
+++ Assets/menuHand.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuHand : MonoBehaviour
+{
+    public anyKey startLevel;
+    public string axisName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        startLevel.change();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis(axisName) * Time.deltaTime, 0, 0);
+    }
+}
