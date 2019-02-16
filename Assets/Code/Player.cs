0fd7fa57195422703483e04d5dda7a389bd01d7a
bjorngamedev <bjorn@gamedevcompany.com>
2019-02-16 15:13:14 +0200

    Collider test.

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -4,25 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
-    private QuantumManager _quantumMananger;
     // Start is called before the first frame update
     void Start()
     {
-        _quantumMananger = FindObjectOfType<QuantumManager>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3[] positions = _quantumMananger.plotPositions;
-        if (positions.Length > 0)
-        {
-            int i = Mathf.RoundToInt(((transform.position.x + 20f) / 40f) * positions.Length);
-            transform.position = new Vector3(transform.position.x, positions[i].y, 0);
-
-
-
-        }
+        
     }
 }
