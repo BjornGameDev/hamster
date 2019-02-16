5b9fa05723c88725ef2e1536140dfa4a5501345a
aarree <aarreentertainment@gmail.com>
2019-02-16 16:49:14 +0200

    particle and inBut

---
+++ Assets/particleRandomizer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleRandomizer : MonoBehaviour
+{
+    public QuantumManager qm;
+
+    int sizeIndex;
+    int speedIndex;
+    int emissionIndex;
+    int rindex;
+    int gindex;
+    int bindex;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+    }
+}
