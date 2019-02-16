bd9088334f55416a4708f4426af8fe2b169325f3
bjorngamedev <bjorn@gamedevcompany.com>
2019-02-16 13:46:58 +0200

    X position of player.

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -15,6 +15,11 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3[] positions = _quantumMananger.plotPositions;
+        if (positions.Length > 0)
+        {
+            int i = Mathf.RoundToInt(((transform.position.x + 20f) / 40f) * positions.Length);
+            transform.position = new Vector3(transform.position.x, positions[i].y, 0);
+        }
     }
 }
