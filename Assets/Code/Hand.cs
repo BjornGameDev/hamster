e0ffb9877b5ff6daf11576c50abf6f17418cba0a
bjorngamedev <bjorn@gamedevcompany.com>
2019-02-17 10:20:00 +0200

    Updated hands.

--- Assets/Code/Hand.cs
+++ Assets/Code/Hand.cs
@@ -9,7 +9,6 @@ public class Hand : MonoBehaviour
     private Vector3 _initialPosition;
     public float speed = 15;
     private float verticalSpeed = 1;
-    public AnimationCurve animationCurve;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,14 +22,7 @@ public class Hand : MonoBehaviour
         if (_quantumMananger.plotPositions.Length > 0)
         {
             float wellPosition = _quantumMananger.getWellPosition(handName);
-
-            if(handName == "leftHand") {
-                //verticalSpeed = Mathf.Clamp(verticalSpeed + (Time.deltaTime * Input.GetAxis("LeftHandSpeed") * -1), 1, 3);
-            } else {
-              //  verticalSpeed = Mathf.Clamp(verticalSpeed + (Time.deltaTime * Input.GetAxis("RightHandSpeed") * -1), 1, 3);
-
-            }
-            transform.position = new Vector3(_initialPosition.x + (wellPosition * speed), _initialPosition.y , transform.position.z);
+            transform.position = new Vector3(_initialPosition.x + (wellPosition * 50), _initialPosition.y , transform.position.z);
 
         }
     }
